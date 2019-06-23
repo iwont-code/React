bf082636d54ac0f1cc590cfd9dea3b9eaa71aa6d
toby <toby3325@163.com>
2019-06-23 22:13:35 +0800

    实现tabbar和路由结合

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import "./styles/App.css";
+// 引入layout组件
+import Mylayout from "./compoments/Mylayout";
+
+// 引入路由组件
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Mine from "./pages/Mine";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: "redTab",
+      hidden: false,
+      fullScreen: false
+    };
+  }
+  render() {
+    return (
+      <Fragment>
+        <Router>
+          {/* 首页 */}
+          <Route
+            path="/"
+            exact
+            render={props => (
+              <Mylayout {...props}>
+                <Home />
+              </Mylayout>
+            )}
+          />
+          {/* 购物车 */}
+          <Route
+            path="/Cart"
+            render={props => (
+              <Mylayout {...props}>
+                <Cart />
+              </Mylayout>
+            )}
+          />
+          {/* 我的 */}
+          <Route
+            path="/Mine"
+            render={props => (
+              <Mylayout {...props}>
+                <Mine />
+              </Mylayout>
+            )}
+          />
+        </Router>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/compoments/Mylayout.js
@@ -0,0 +1,55 @@
+import React,{Component} from "react";
+
+import { TabBar } from "antd-mobile";
+
+class Mylayout extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedTab: "redTab",
+        hidden: false,
+        fullScreen: false
+      };
+    }
+    render() {
+      return (
+        <div
+          style={{ position: "fixed", height: "100%", width: "100%", top: "0" }}
+        >
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="white"
+          >
+            <TabBar.Item
+              title="首页"
+              key="Home"
+              icon={<span className="iconfont icon-home" />}
+              selectedIcon={<span className="iconfont icon-home" />}
+              selected={this.props.match.url==="/"}
+              onPress={() => { this.props.history.push("/") }}
+            >{this.props.children}</TabBar.Item>
+            <TabBar.Item
+              icon={<span className="iconfont icon-gouwuche" />}
+              selectedIcon={<span className="iconfont icon-gouwuche" />}
+              title="购物车"
+              key="Cart"
+              selected={this.props.match.url==='/Cart'}
+              onPress={() => { this.props.history.push('/Cart')}}
+            >{this.props.children}</TabBar.Item>
+            <TabBar.Item
+              icon={<span className="iconfont icon-weibiaoti2fuzhi12"></span>}
+              selectedIcon={<span className="iconfont icon-weibiaoti2fuzhi12" />}
+              title="我的"
+              key="Mine"
+              selected={this.props.match.url==='/Mine'}
+              onPress={() => { this.props.history.push('/Mine')}}
+            >{this.props.children}</TabBar.Item>
+          </TabBar>
+        </div>
+      );
+    }
+  }
+  export default Mylayout;
+  
+  
