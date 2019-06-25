fbf3327ea719b2cc161eec1ef31478357ee187f2
toby <toby3325@163.com>
2019-06-25 21:16:07 +0800

    完成首页的渲染

--- src/api/index.js
+++ src/api/index.js
@@ -4,14 +4,20 @@ import axios from "axios";
 axios.defaults.baseURL = "http://react.zbztb.cn/site/";
 
 // 添加响应拦截器 请求成功后会被调用的一个方法
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(
+  function(response) {
     // 对响应数据做点什么
     // console.log(response.data)
     return response.data;
-  }, function (error) {
+  },
+  function(error) {
     // 对响应错误做点什么
     return Promise.reject(error);
-  });
+  }
+);
 
-// 获取首页轮播图数据
+// 获取首页轮播图 + 推荐商品数据
 export const getGoods = () => axios.get("goods/gettopdata/goods");
+
+// 获取首页商品列表数据
+export const getGoodsList = () => axios.get("goods/getgoodsgroup");
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,29 +3,33 @@ import React, { Component, Fragment } from "react";
 import { Carousel, WingBlank } from "antd-mobile";
 
 // 调用API接口
-import { getGoods } from "../api";
+import { getGoods, getGoodsList } from "../api";
 
 class Home extends Component {
   state = {
     sliderlist: [],
-    imgHeight: 176
+    imgHeight: 176,
+    toplist: [],
+    goodslist: []
   };
   componentDidMount() {
     getGoods().then(result => {
       if (result.status === 0) {
         this.setState({ sliderlist: result.message.sliderlist });
-        console.log(result);
+        // console.log(result);
+        this.setState({ toplist: result.message.toplist });
       }
+    });
+    getGoodsList().then(result => {
+      this.setState({ goodslist: result.message });
     });
   }
   render() {
     return (
       <Fragment>
+        {/* 轮播图部分开始 */}
         <WingBlank>
-          <Carousel
-            autoplay
-            infinite
-          >
+          <Carousel autoplay infinite>
             {this.state.sliderlist.map(val => (
               <a
                 key={val.id}
@@ -49,6 +53,140 @@ class Home extends Component {
               </a>
             ))}
           </Carousel>
+          {/* 轮播图部分结束 */}
+          {/* 推荐商品部分开始 */}
+          <div className="recommend_row">
+            <div className="recommend_row_title">推荐商品</div>
+            <div className="recommend_row_content">
+              {this.state.toplist.map(v => (
+                <a href="javascript:;" className="top_item" key={v.id}>
+                  <div className="top_item_img">
+                    <img src={v.img_url} />
+                  </div>
+                  <div className="top_item_name">
+                    <p>{v.title}</p>
+                  </div>
+                </a>
+              ))}
+            </div>
+            <style jsx>{`
+              .recommend_row_title {
+                background-color: #ccc;
+                color: #666;
+                font-size: 14px;
+                padding: 8px;
+                margin-top: 5px;
+              }
+              .recommend_row_content {
+                .top_item {
+                  display: flex;
+                  background-color: #fff;
+                  border-bottom: 1px solid #ccc;
+                  .top_item_img_wrap {
+                    width: 20%;
+                    padding: 20px;
+                    img {
+                      width: 80%;
+                    }
+                  }
+                  .top_item_name {
+                    width: 80%;
+                    display: flex;
+                    align-items: center;
+                    p {
+                      text-overflow: ellipsis;
+                      overflow: hidden;
+                      white-space: nowrap;
+                    }
+                  }
+                }
+              }
+            `}</style>
+          </div>
+
+          {/* 推荐商品部分结束 */}
+          {/* 商品列表部分开始 */}
+          <div className="goods_list">
+            {this.state.goodslist.map(v => {
+              return (
+                <div key={v.level1cateid} className="goods_item">
+                  <div className="goods_title">{v.catetitle}</div>
+                  <div className="goods_content">
+                    {v.datas.map(v2 => (
+                      <a href="#" key={v2.artID}>
+                        <img src={v2.img_url} alt="" />
+                        <div className="art_title">{v2.artTitle}</div>
+                        <div className="goods_price">
+                          <span className="sell_price">{v2.sell_price}</span>
+                          <span className="market_price">
+                            {v2.market_price}
+                          </span>
+                        </div>
+                        <div className="hot_sell">
+                          热卖中 <span>{v2.stock_quantity}</span>
+                        </div>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+
+            <style jsx>{`
+              .goods_item {
+                .goods_title {
+                  background-color: #f5f5f9;
+                  color: #666;
+                  font-size: 14px;
+                  padding: 8px;
+                }
+                .goods_content {
+                  display: flex;
+                  flex-wrap: wrap;
+                  > a {
+                    width: 50%;
+                    border-bottom: 1px solid #ccc;
+                    background-color: #fff;
+                    padding: 5px;
+                    &:nth-child(odd) {
+                      border-right: 1px solid #ccc;
+                    }
+                    img {
+                      width: 80%;
+                      margin: 0 auto;
+                    }
+                    .art_title {
+                      font-size: 12px;
+                      color: #666;
+                      padding: 5px 0;
+                    }
+                    .goods_price {
+                      display: flex;
+                      justify-content: space-between;
+                      padding: 5px 0;
+                      .sell_price {
+                        font-size: 12px;
+                        color: red;
+                      }
+                      .market_price {
+                        font-size: 12px;
+                        color: #ccc;
+                        text-decoration: line-through;
+                      }
+                    }
+                    .hot_sell {
+                      padding: 5px 0;
+                      span {
+                        color: red;
+                        font-size: 14px;
+                      }
+                    }
+                  }
+                }
+              }
+            `}</style>
+          </div>
+          {/* 商品列表部分结束 */}
         </WingBlank>
       </Fragment>
     );
